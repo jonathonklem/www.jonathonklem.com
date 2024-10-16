d58b6e9fd1e64d6e0a55bd9638277fa75e8f72ae
Jonathon Klem <jonathonklem@gmail.com>
2024-10-16 15:42:54 -0500

    Latest updates

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,7 +13,7 @@ const Footer: React.FC<PageProps> = () => {
                     </a>
                 </li>
                 <li className="inline-block list-none mx-8">
-                    <a href='http://jonathonklem.com/assets/jonathonklem-resume.pdf'>
+                    <a href='https://ManageMemberships.com' target='_blank'>
                         <svg id="resume" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <rect x="2" y="2" width="20" height="20" rx="2" ry="2"></rect>
                             <line x1="12" y1="18" x2="12" y2="18"></line>
